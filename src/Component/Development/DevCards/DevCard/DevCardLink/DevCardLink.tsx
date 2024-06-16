f780273aeed6aba8541a4f5848371914a8e76637
Sam <screasey97@googlemail.com>
2024-06-17 11:27:59 +1200

    add prettier tailwind plugin

--- src/Component/Development/DevCards/DevCard/DevCardLink/DevCardLink.tsx
+++ src/Component/Development/DevCards/DevCard/DevCardLink/DevCardLink.tsx
@@ -6,10 +6,10 @@ export const DevCardLink = ({ children, href }: DevCardLinkProps) => (
     <a
         href={href}
         rel="noopener noreferrer"
-        className="tw-font-lora tw-px-3 tw-py-2 tw-border tw-rounded tw-transition-all tw-shadow-md tw-w-[150px] hover:tw-w-[170px] focus-visible:tw-w-[170px] tw-relative tw-text-left tw-group/link"
+        className="tw-group/link tw-relative tw-w-[150px] tw-rounded tw-border tw-px-3 tw-py-2 tw-text-left tw-font-lora tw-shadow-md tw-transition-all hover:tw-w-[170px] focus-visible:tw-w-[170px]"
         target="_blank"
     >
         {children}
-        <span className="tw-absolute tw-hidden group-hover/link:tw-inline-block group-focus-visible/link:tw-inline-block tw-w-0 tw-h-0 tw-right-0 tw-top-1/2 tw--translate-y-1/2 tw-border-[10px] tw-border-[transparent] tw-border-l-light-green" />
+        <span className="tw-absolute tw-right-0 tw-top-1/2 tw-hidden tw-h-0 tw-w-0 tw--translate-y-1/2 tw-border-[10px] tw-border-[transparent] tw-border-l-light-green group-hover/link:tw-inline-block group-focus-visible/link:tw-inline-block" />
     </a>
 );
