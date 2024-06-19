0ed02a842b89d8d0ae88f094e836e3b95158e096
Sam <screasey97@googlemail.com>
2024-06-19 21:09:05 +1200

    encode uri component

--- src/Common/NetflifyImage/NetlifyImage.tsx
+++ src/Common/NetflifyImage/NetlifyImage.tsx
@@ -31,7 +31,7 @@ const loadImageSrc = (src: string) =>
 
 const convertUrlToNetlifyUrl = (url: string, width?: number) => {
     if (process.env.NODE_ENV === 'production') {
-        let netlifyUrl = `/.netlify/images?url=${url}`;
+        let netlifyUrl = `/.netlify/images?url=${encodeURIComponent(url)}`;
 
         if (width) {
             netlifyUrl += `&width=${width * Math.ceil(window.devicePixelRatio)}`;
