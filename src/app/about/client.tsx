2990823b521e4824d0b847facc7826ddb5d12937
Sam <screasey97@googlemail.com>
2024-06-26 21:32:34 +1200

    added metadata

---
+++ src/app/about/client.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { faInstagram, faFacebook, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
+
+import { ContactLink } from '../../Feature/About/ContactLink/ContactLink';
+
+const ABOUT_VARIANTS = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.3,
+        },
+    },
+};
+
+const ABOUT_CONTENT_VARIANTS = { hidden: { opacity: 0, y: 30 }, show: { opacity: 1, y: 0 } };
+
+export default function AboutClient() {
+    return (
+        <motion.div
+            className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-py-10"
+            variants={ABOUT_VARIANTS}
+            initial="hidden"
+            animate="show"
+        >
+            <div className="tw-flex tw-w-[95%] tw-max-w-[1100px] tw-flex-col tw-items-center tw-gap-12 tw-px-2 lg:tw-flex-row">
+                <motion.img
+                    variants={ABOUT_CONTENT_VARIANTS}
+                    className="tw-aspect-square tw-w-[75vw] tw-max-w-[350px] tw-rounded-full tw-object-cover"
+                    src="/about/about-mountain.jpg"
+                    transition={{ type: 'tween' }}
+                    height={1000}
+                    width={1000}
+                />
+                <div className="tw-text-center lg:tw-text-left">
+                    <motion.h2
+                        className="tw-mb-4 tw-font-lora tw-text-4xl tw-font-bold tw-text-green"
+                        variants={ABOUT_CONTENT_VARIANTS}
+                        transition={{ type: 'tween' }}
+                    >
+                        Hi! I&apos;m Sam
+                    </motion.h2>
+
+                    <motion.div variants={ABOUT_CONTENT_VARIANTS} transition={{ type: 'tween' }}>
+                        <p className="tw-mb-3 tw-font-lora">
+                            I&apos;ve spent the last 9 years travelling full-time, working remotely as a software
+                            engineer and photographer.
+                        </p>
+                        <p className="tw-mb-3 tw-font-lora">
+                            My portfolio is a showcase of everything I have taught myself. I started my journey into web
+                            development after discovering that it allowed me to combine creativity with my natural
+                            problem-solving skillset.
+                        </p>
+                        <p className="tw-mb-3 tw-font-lora">
+                            With 5 years of professional experience I can bring your website ideas to life, and with 7
+                            years of hospitality experience I can guarantee great communication all the way.
+                        </p>
+                        <p className="tw-mb-3 tw-font-lora">Reach out using any of the links below!</p>
+                    </motion.div>
+                    <motion.div
+                        className="tw-mt-6 tw-flex tw-w-full tw-justify-around lg:tw-max-w-[500px] lg:tw-justify-between"
+                        transition={{ type: 'tween' }}
+                        variants={ABOUT_CONTENT_VARIANTS}
+                    >
+                        <ContactLink href="mailto:screasey97@gmail.com" label="Email" icon={faEnvelope} />
+                        <ContactLink
+                            href="https://www.instagram.com/venturegreenphotography/"
+                            label="Instagram"
+                            animationDelay="2s"
+                            icon={faInstagram}
+                        />
+                        <ContactLink
+                            href="https://www.facebook.com/sam.creasey1"
+                            icon={faFacebook}
+                            label="Facebook"
+                            animationDelay="4s"
+                        />
+                        <ContactLink
+                            href="https://www.linkedin.com/in/sam-creasey-056b371b7/"
+                            label="LinkedIn"
+                            animationDelay="6s"
+                            icon={faLinkedinIn}
+                        />
+                        <ContactLink
+                            href="https://github.com/08screasey"
+                            label="Github"
+                            animationDelay="8s"
+                            icon={faGithub}
+                        />
+                    </motion.div>
+                </div>
+            </div>
+        </motion.div>
+    );
+}
--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -1,114 +1,11 @@
-'use client';
-
-import { faFile } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { motion } from 'framer-motion';
 import { type Metadata } from 'next';
-import Link from 'next/link';
-import { PropsWithChildren, useEffect, useRef } from 'react';
 
-import { TypingEffect } from '../../Common/TypingEffect';
-import { DEV_CARD_VARIANTS } from '../../Feature/Development/DevCards/DevCard/animations';
-import { DevCards } from '../../Feature/Development/DevCards/DevCards';
-import { getDevCardVariants } from '../../Feature/Development/DevCards/animations';
-import { FOCUS_VISIBLE_STYLES } from '../../Utility/focusStyles';
-import { merge } from '../../Utility/merge';
-import { TypingEffectProps, convertNodesToSpanElements } from '../../Utility/useTypingEffect';
-import { WORK_EXPERIENCE, PROJECT_EXPERIENCE } from '../../data/experience';
-
-const CONTENT: TypingEffectProps['content'] = [
-    { text: '<', className: 'tw-text-cyan' },
-    { text: 'p', className: 'tw-text-red' },
-    { text: '>', className: 'tw-text-cyan' },
-    {
-        text: ' With a mathematical background, I have always excelled in problem solving. Web development for me is a place where I can apply those skills with creativity. ',
-    },
-    { text: '</', className: 'tw-text-cyan' },
-    { text: 'p', className: 'tw-text-red' },
-    { text: '>', className: 'tw-text-cyan' },
-    { text: '\n', className: 'tw-block tw-w-full tw-h-2' },
-    { text: '<', className: 'tw-text-cyan' },
-    { text: 'p', className: 'tw-text-red' },
-    { text: '>', className: 'tw-text-cyan' },
-    {
-        text: ' Check out some of my projects below! ',
-    },
-    { text: '</', className: 'tw-text-cyan' },
-    { text: 'p', className: 'tw-text-red' },
-    { text: '>', className: 'tw-text-cyan' },
-];
-
-const DEVELOPMENT_VISITED_KEY = 'development-visited';
-
-const DevelopmentHeader = ({ children }: PropsWithChildren) => (
-    <motion.h3
-        variants={DEV_CARD_VARIANTS}
-        transition={{ type: 'tween' }}
-        className="tw-mb-6 tw-mt-12 tw-w-full tw-border-b tw-border-solid tw-border-b-grey tw-text-left tw-font-lora tw-text-2xl tw-font-bold tw-text-green"
-    >
-        {children}
-    </motion.h3>
-);
+import DevelopmentClient from './client';
 
 export const metadata: Metadata = {
     title: "Sam Creasey's Development Portfolio",
 };
 
 export default function DevelopmentPage() {
-    const hasVisited =
-        useRef(typeof window !== undefined ? sessionStorage.getItem(DEVELOPMENT_VISITED_KEY) === 'true' : false)
-            .current ?? false;
-
-    useEffect(() => {
-        sessionStorage.setItem(DEVELOPMENT_VISITED_KEY, 'true');
-    }, []);
-
-    return (
-        <motion.div
-            variants={getDevCardVariants(hasVisited ? 0 : 3.2)}
-            initial="hidden"
-            animate="show"
-            className="tw-w-full tw-bg-[url(/.netlify/images?url=/development/background.jpg)] tw-bg-cover tw-bg-fixed tw-bg-center tw-bg-no-repeat tw-px-2 tw-pb-[120px] tw-pt-12 md:tw-px-10"
-        >
-            <motion.h2
-                className="tw-text-center tw-font-lora tw-text-4xl tw-font-bold tw-text-green"
-                initial={DEV_CARD_VARIANTS.hidden}
-                animate={DEV_CARD_VARIANTS.show}
-                transition={{ type: 'tween' }}
-            >
-                Web Development
-            </motion.h2>
-            <div className="tw-mx-auto tw-max-w-[600px] tw-px-4 tw-pt-4">
-                <p className="tw-min-h-[100px] tw-text-center tw-font-code">
-                    {hasVisited ? (
-                        <motion.span variants={DEV_CARD_VARIANTS} transition={{ type: 'tween' }}>
-                            {convertNodesToSpanElements(CONTENT)}
-                        </motion.span>
-                    ) : (
-                        <TypingEffect content={CONTENT} enabled minSpeed={8} maxSpeed={18} />
-                    )}
-                </p>
-            </div>
-            <div className="tw-relative tw-mx-auto tw-w-full tw-max-w-[1100px]">
-                <DevelopmentHeader>Work Experience</DevelopmentHeader>
-                <DevCards devCards={WORK_EXPERIENCE} />
-                <div className="tw-mx-auto tw-flex tw-w-[90vw] tw-max-w-[500px] tw-flex-col tw-items-center tw-gap-1 tw-rounded tw-border-grey tw-bg-black/50 tw-px-6 tw-py-4 tw-font-active tw-text-4xl tw-text-white tw-shadow lg:tw-px-10 lg:tw-py-8">
-                    Want more information?
-                    <Link
-                        href="/Sam Creasey Resume.pdf"
-                        target="_blank"
-                        className={merge(
-                            'tw-border-line tw-group/resume tw-inline-flex tw-items-center tw-gap-x-2 tw-rounded-sm tw-px-2 tw-py-0 tw-font-lora tw-text-2xl tw-text-white tw-underline tw-transition hover:tw-scale-105 hover:tw-text-light-grey',
-                            FOCUS_VISIBLE_STYLES,
-                        )}
-                    >
-                        <FontAwesomeIcon icon={faFile} />
-                        View Resume
-                    </Link>
-                </div>
-                <DevelopmentHeader>Development Projects</DevelopmentHeader>
-                <DevCards devCards={PROJECT_EXPERIENCE} />
-            </div>
-        </motion.div>
-    );
+    return <DevelopmentClient />;
 }
