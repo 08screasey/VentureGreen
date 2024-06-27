1ce7cb89a4d4434eb06400ea30aadcbf8401616e
Sam <screasey97@googlemail.com>
2024-06-27 19:48:14 +1200

    add preload to stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <head>
-                <link rel="preload" href="https://use.typekit.net/zkq8euy.css" as="style" />
+                <link
+                    rel="preload stylesheet"
+                    href="https://use.typekit.net/zkq8euy.css"
+                    as="style"
+                    crossOrigin="anonymous"
+                />
             </head>
             <body>
                 <Layout>{children}</Layout>
